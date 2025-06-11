6efa272efc1edf98d8a12d52bb29b6d18c93212b
Iron-Bum <chasmonahtus@gmail.com>
2025-06-11 12:58:33 +0300

    v1

--- mysite/example2/url.py
+++ mysite/example2/url.py
@@ -3,4 +3,6 @@ from example2 import views
 
 urlpatterns = [
     path('', views.main_page),
+    path('card/', views.card),
+    path('market/', views.market),
 ]
--- mysite/example2/views.py
+++ mysite/example2/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 
 def main_page(request):
-    return render(request, 'main_page/main_page.html')
+    data = {'title': 'Глвная страница'}
+    return render(request, 'main_page/main_page.html', data)
+
+
+def card(request):
+    data = {'title': 'Card'}
+    return render(request, 'main_page/card.html', data)
+
+
+def market(request):
+    products = [
+        {'name': 'smartphone', 'cost': 70000},
+        {'name': 'XBox', 'cost': 110000},
+        {'name': 'VR', 'cost': 55000}
+    ]
+    data = {'title': 'Market', 'products': products}
+    return render(request, 'main_page/market.html', context=data)
